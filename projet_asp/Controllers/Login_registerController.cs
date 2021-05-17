e4da3d27a4d0d737f7e37b42b06559e323d8906d
amina4576782 <aminabourabeh@gmail.com>
2021-05-17 16:04:14 +0100

    userform

---
+++ projet_asp/Controllers/Login_registerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_asp.Controllers
+{
+    public class Login_registerController : Controller
+    {
+        // GET: Login_register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
